689ef47bb85d039d6072077060f5d27eb4d9bc0f
Sany655 <mazharulalam26@gmail.com>
2022-04-22 15:27:08 +0600

    a

--- server/app.js
+++ server/app.js
@@ -1,15 +1,24 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
-const io = require("socket.io")
-
+const { Server } = require("socket.io")
+const http = require("http")
 app.use(cors())
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: ["http://192.168.0.116:3000", "http://localhost:3000"]
+        // methods: ["GET", "POST"]
+    }
+})
 
 app.get('/',(req,res) => {
     res.send("hello world")
 })
 
-// io.Server({})
+io.on("connection", () => {
+    
+})
 
 app.listen(5000,() => {
     console.log("http://localhost:5000");
