fb923949e7bce96738d06dfd23b9b03ff7a7a667
Sany655 <mazharulalam26@gmail.com>
2022-04-25 05:56:45 +0600

    accomplished

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 const express = require("express")
 const app = express()
-const cors = require("cors")
 const { Server } = require("socket.io")
 const http = require("http")
 const server = http.createServer(app)
@@ -10,7 +9,6 @@ const io = new Server(server, {
     }
 })
 
-// app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
