2e83d86fc429a726774e61aab88d1dcfbea5be94
Sany655 <mazharulalam26@gmail.com>
2022-05-01 12:44:23 +0600

    private route set

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { BrowserRouter, Navigate, Route, Routes, useLocation } from 'react-router-dom'
+import Login from './routes/pages/Login'
 import Routers from './routes/Routers'
 
 const App = () => {
+    const user = useSelector((store) => store.user).user;
+
+    // useEffect(() => {
+    //     if (auth) {
+    //         socket.emit("loggedIn", { _id: user._id })
+    //     }
+    // }, [auth])
+
+    function PrivateRoute({ children }) {
+        const location = useLocation();
+        if (!Object.keys(user).length) {
+            return <Navigate to="/login" state={{ from: location }} replace />;
+        }
+        return children;
+    }
+
+    function PublicRoute({ children }) {
+        const location = useLocation();
+        if (Object.keys(user).length) {
+            return <Navigate to="/" state={{ from: location }} replace />;
+        }
+        return children;
+    }
+
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Routers />} />
+                <Route path='/' element={<PrivateRoute><Routers /></PrivateRoute>} />
+                <Route path='/login' element={<PublicRoute><Login /></PublicRoute>} />
             </Routes>
         </BrowserRouter>)
 }
